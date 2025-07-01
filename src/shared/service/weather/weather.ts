5a0ec4a7923deee9bc5d92516bc1b2dccd001f79
alexandre-silvaa <alexandrebsilva9@gmail.com>
2025-07-01 00:03:01 -0300

    fix: 🐛 destructure api response

--- src/shared/service/weather/weather.ts
+++ src/shared/service/weather/weather.ts
@@ -3,7 +3,7 @@ import { IGetWeatherReqParams } from './interface';
 import { api } from '../instance';
 
 const getCurrentWeather = async (params: IGetWeatherReqParams): Promise<AxiosResponse> => {
-  const data = await api.get('/weather', { params: { ...params, lang: 'pt_br', units: 'metric' } });
+  const { data } = await api.get('/weather', { params: { ...params, lang: 'pt_br', units: 'metric' } });
   return data;
 };
 
